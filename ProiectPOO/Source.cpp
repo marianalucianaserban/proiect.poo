04df5b0d8c6dddf9efcc383d91263ad6b891915d
marianalucianaserban <serbanmariana22@stud.ase.ro>
2023-11-30 16:56:06 +0200

    incercare

--- ProiectPOO/Source.cpp
+++ ProiectPOO/Source.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 using namespace std;
 
+
+class Calcul
+{
+public:
+	string expresie;
+
+	Calcul()
+	{
+
+	}
+
+	void gasesteParantezele()
+	{
+		for (int i = 0; i < 10; i++)
+		{
+			if (expresie[i] == '(')
+				int beginP = i;
+			if (expresie[i] == ')')
+				int endP = i;
+	    }
+	}
+};
 int main()
 {
-	cout << endl << "Hello";
+	Calcul c;
+	cout << endl << "Introduceti expresia: ";
+	cin >> c.expresie;
+	cout << endl << c.expresie;
 } 
