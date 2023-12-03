ac18e8f4431c5ab4abc10abc9f2312c9309d084b
marianalucianaserban <serbanmariana22@stud.ase.ro>
2023-12-03 15:49:51 +0200

    h si cpp

--- ProiectPOO/Calculator.cpp
+++ ProiectPOO/Calculator.cpp
@@ -1,98 +1,78 @@
-#include <iostream>
-using namespace std;
-
-class Calculator {
-
-private:
-    bool readyForAction;
-    static int nrCalcule;
-    char* expresii;
-public:
-    Calculator() : readyForAction(false), expresii(nullptr)
-    {
-    }
-    Calculator(const char* expresii) : readyForAction(false), expresii(nullptr)
-    {
-        setExpresii(expresii);
-    }
-    ~Calculator()
-    {
-        delete[] expresii;
-
-    }
-    Calculator(const Calculator& calcul)
-    {
-
-    }
-
-    void gasesteParantezele()
-    {
-        for (int i = 0; i < strlen(expresii); i++)
-        {
-            if (expresii[i] == '(')
-                int beginP = i;
-            if (expresii[i] == ')')
-                int endP = i;
-        }
-    }
-    Calculator& operator=(const Calculator& calcul)
-    {
-        if (this != &calcul) {
-            readyForAction = calcul.readyForAction;
-            setExpresii(calcul.expresii);
-        }
-        return *this;
-    }
-
-    const char* getExpresii()
-    {
-        return expresii;
-    }
-
-    void setExpresii(const char* expresii)
-    {
-        delete[] this->expresii;
-        this->expresii = new char[strlen(expresii) + 1];
-        strcpy_s(this->expresii, strlen(expresii) + 1 ,expresii);
-    }
-    
-    double folosesteOperator(double a, double b, char Operator)
-    {
-        switch (Operator) {
-        case '+':
-            return a + b;
-        case '-':
-            return a - b;
-        case '*':
-            return a * b;
-        case '/':
-            return a / b;
-        default:
-            throw exception("!!!Operator invalid!!!");
-        }
-    } 
-    void ALTA_METODA();
-
-
-    bool operator==(const Calculator& calcul)
-    {
-        return (strcmp(this->expresii, calcul.expresii) == 0);
-    }
-
-    char operator[](int index)
-    {
-        if (expresii && index >= 0 && index < static_cast<int>(strlen(expresii)))
-        {
-            return expresii[index];
-        }
-        else
-            throw exception("Indexul nu e bun!");
-            //tratam cazul in care indexul nu  e bun
-
-    }
-
-    friend ostream& operator<<(ostream& out, const Calculator& calculator);
-    friend istream& operator>>(istream& in, Calculator& calculator);
-};
+#include "Calculator.h"
+#include <string>
 
 int Calculator::nrCalcule = 0;
+
+Calculator::Calculator() : readyForAction(false), expresii(nullptr)
+{
+}
+
+Calculator::Calculator(const char* expresii) : readyForAction(false), expresii(nullptr)
+{
+    setExpresii(expresii);
+}
+
+Calculator::~Calculator()
+{
+    delete[] expresii;
+}
+
+Calculator::Calculator(const Calculator& calcul)
+{
+}
+
+void Calculator::gasesteParantezele()
+{
+}
+
+Calculator& Calculator::operator=(const Calculator& calcul)
+{
+    if (this != &calcul) {
+        readyForAction = calcul.readyForAction;
+        setExpresii(calcul.expresii);
+    }
+    return *this;
+}
+
+const char* Calculator::getExpresii()
+{
+    return expresii;
+}
+
+void Calculator::setExpresii(const char* expresii)
+{
+    delete[] this->expresii;
+    this->expresii = new char[strlen(expresii) + 1];
+    strcpy_s(this->expresii, strlen(expresii) + 1, expresii);
+}
+
+void Calculator::ALTA_METODA()
+{
+}
+
+bool Calculator::operator==(const Calculator& calcul)
+{
+    return (strcmp(this->expresii, calcul.expresii) == 0);
+}
+
+char Calculator::operator[](int index)
+{
+    if (expresii && index >= 0 && index < static_cast<int>(strlen(expresii))) {
+        return expresii[index];
+    }
+    else {
+        throw std::exception("Indexul nu este valid!");
+    }
+}
+
+std::ostream& operator<<(std::ostream& out, const Calculator& calculator)
+{
+
+    return out;
+}
+
+std::istream& operator>>(std::istream& in, Calculator& calculator)
+{
+
+    return in;
+}
--- ProiectPOO/Calculator.h
+++ ProiectPOO/Calculator.h
@@ -1,29 +1,28 @@
 #pragma once
-
 #include <iostream>
-using namespace std;
 
 class Calculator {
 private:
-    char* expresii; 
+    bool readyForAction;
+    static int nrCalcule;
+    char* expresii;
+
 public:
-    Calculator(); // Constructor implicit
-    Calculator(const char* expresii); // Constructor cu parametri
-    ~Calculator(); // Destructor
-    Calculator(const Calculator& calcul); // Constructor de copiere
-    Calculator& operator=(const Calculator& calcul); 
-    // Metode de acces si setteri pentru atribut
-    const char* getExpresii();
-    void setExpresii(const char* expresii);
-    // metode generice
-    void FACEM_METODEGENERICE();
-    void ALTA_METODA();
+    Calculator();
+    Calculator(const char* expresii);
+    ~Calculator();
+    Calculator(const Calculator& calcul);
 
+    void gasesteParantezele();
+
+    Calculator& operator=(const Calculator& calcul);
     bool operator==(const Calculator& calcul);
-
     char operator[](int index);
 
-    friend ostream& operator<<(ostream& out, const Calculator& calculator);
-    friend istream& operator>>(istream& in, Calculator& calculator);
+    const char* getExpresii();
+    void setExpresii(const char* expresii);
+    void ALTA_METODA();
 
+    friend std::ostream& operator<<(std::ostream& out, const Calculator& calculator);
+    friend std::istream& operator>>(std::istream& in, Calculator& calculator);
 };
